92aeee8cdeb1d161cbbfbcd6b266517ce6836338
Rasan Handaragama OU <RASANH.OU@mobitel.lk>
2025-06-06 16:07:40 +0530

    plan form frontend finish

--- app/static/scripts/plan_form.js
+++ app/static/scripts/plan_form.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     editableElements.forEach(({ element, label }) => {
         element.addEventListener("click", () => {
-            showEditPopUp(element, label);
+            new popup(element, label);
         });
     });
 
@@ -36,214 +36,188 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+class popup {
+    constructor(element, label) {
+        document.getElementById("title_section").style.opacity = "0.5";
+        document.getElementById("title_section").style.transition = "0.3s";
 
-function showEditPopUp(element, label) {
+        document.getElementById("form_container").style.opacity = "0.5";
+        document.getElementById("form_container").style.transition = "0.3s";
 
-    document.getElementById("title_section").style.opacity = "0.5";
-    document.getElementById("title_section").style.transition = "0.3s";
+        this.inputWrapper = document.createElement("div");
+        this.inputWrapper.classList.add("input_wrapper");
+        this.inputWrapper.style.position = "absolute";
+        this.inputWrapper.style.display = "flex";
+        this.inputWrapper.style.flexDirection = "column";
+        this.inputWrapper.style.justifyContent = "center";
+        this.inputWrapper.style.alignItems = "center";
+        this.inputWrapper.style.top = "30%";
+        this.inputWrapper.style.left = "40%";
+        this.inputWrapper.style.width = "400px";
+        this.inputWrapper.style.height = "30vh";
+        this.inputWrapper.style.border = "1px solid #ccc";
+        this.inputWrapper.style.borderRadius = "10px";
+        this.inputWrapper.style.backgroundColor = "white";
+        this.inputWrapper.style.zIndex = "1000";
+        this.inputWrapper.innerHTML = "";
 
-    document.getElementById("form_container").style.opacity = "0.5";
-    document.getElementById("form_container").style.transition = "0.3s";
+        this.removeImage = document.createElement("img");
+        this.removeImage.classList.add("remove_icon");
+        this.removeImage.src = "https://jnrcnizvaihqtkeynkjk.supabase.co/storage/v1/object/public/webimages/icons/close-line-svgrepo-com.svg";
+        this.removeImage.alt = "Remove";
+        this.removeImage.style.width = "20px";
+        this.removeImage.style.height = "20px";
+        this.removeImage.style.cursor = "pointer";
+        this.removeImage.style.position = "absolute";
+        this.removeImage.style.top = "0";
+        this.removeImage.style.right = "0";
 
+        this.labelText = document.createElement("label");
+        this.labelText.setAttribute("for", element.id);
+        this.labelText.innerText = label;
+        this.labelText.style.fontSize = "20px";
+        this.labelText.style.fontWeight = "bold";
+        this.labelText.style.marginBottom = "10px";
 
-    const inputWrapper = document.createElement("div");
-    inputWrapper.classList.add("input_wrapper");
-    inputWrapper.style.position = "absolute";
-    inputWrapper.style.display = "flex";
-    inputWrapper.style.flexDirection = "column";
-    inputWrapper.style.justifyContent = "center";
-    inputWrapper.style.alignItems = "center";
-    inputWrapper.style.top = "30%";
-    inputWrapper.style.left = "40%";
-    inputWrapper.style.width = "400px";
-    inputWrapper.style.height = "30vh";
-    inputWrapper.style.border = "1px solid #ccc";
-    inputWrapper.style.borderRadius = "10px";
-    inputWrapper.style.backgroundColor = "white";
-    inputWrapper.innerHTML = "";
+        this.inputWrapper.appendChild(this.labelText);
 
-    const removeImage = document.createElement("img");
-    removeImage.classList.add("remove_icon");
-    removeImage.src = "https://jnrcnizvaihqtkeynkjk.supabase.co/storage/v1/object/public/webimages/icons/close-line-svgrepo-com.svg";
-    removeImage.alt = "Remove";
-    removeImage.style.width = "20px";
-    removeImage.style.height = "20px";
-    removeImage.style.cursor = "pointer";
-    removeImage.style.position = "absolute";
-    removeImage.style.top = "0";
-    removeImage.style.right = "0";
+        if (!element) {
+            throw new Error("Element is required to create a popup.");
+        }else if (element.id === "plan_description") {
+            this.descriptionPop = document.createElement("textarea");
+            this.descriptionPop.name = "plan_description";
+            this.descriptionPop.setAttribute("for", element.id);
+            this.descriptionPop.id = "plan_input_textarea";
+            this.descriptionPop.style.display = "flex";
+            this.descriptionPop.style.width = "300px";
+            this.descriptionPop.style.height = "100px";
+            this.descriptionPop.style.border = "1px solid";
+            this.descriptionPop.style.borderRadius = "5px";
 
-    const labelText = document.createElement("label");
-    labelText.setAttribute("for", element.id);
-    labelText.innerText = label;
-    labelText.style.fontSize = "20px";
-    labelText.style.fontWeight = "bold";
-    labelText.style.marginBottom = "10px";
+            this.inputWrapper.appendChild(this.descriptionPop);
 
-    inputWrapper.appendChild(labelText);
+        }else if (element.id === "plan_price" || element.id === "price_with_tax") {
+            this.pricePop = document.createElement("input");
+            this.pricePop.type = "number";
+            this.pricePop.name = "plan_title";
+            this.pricePop.placeholder = label;
+            this.pricePop.id = "plan_input_number"
+            this.pricePop.style.display = "flex";
+            this.pricePop.style.width = "300px";
+            this.pricePop.style.height = "30px";
+            this.pricePop.style.border = "1px solid";
+            this.pricePop.style.borderRadius = "5px";
 
+            this.inputWrapper.appendChild(this.pricePop);
+            
+        }else if (element.id === "plan_img_url") {
+            this.urlPop = document.createElement("input");
+            this.urlPop.type = "url";
+            this.urlPop.name = "plan_image_url";
+            this.urlPop.placeholder = label;
+            this.urlPop.id = "plan_input_url";
+            this.urlPop.style.display = "flex";
+            this.urlPop.style.width = "300px";
+            this.urlPop.style.height = "30px";
+            this.urlPop.style.border = "1px solid";
+            this.urlPop.style.borderRadius = "5px";
 
-    if (element.id === "plan_description") {
-        const textArea = document.createElement("textarea");
-        textArea.name = "plan_description";
-        textArea.setAttribute("for", element.id);
-        textArea.id = "plan_input_textarea";
-        textArea.style.display = "flex";
-        textArea.style.width = "300px";
-        textArea.style.height = "100px";
-        textArea.style.border = "1px solid";
-        textArea.style.borderRadius = "5px";
+            this.inputWrapper.appendChild(this.urlPop);
 
-        inputWrapper.appendChild(textArea);
-        setTimeout(() => textArea.focus(), 0);
+        }else {
+            this.inputPop = document.createElement("input");
+            this.inputPop.type = "text";
+            this.inputPop.name = "plan_title";
+            this.inputPop.placeholder = label;
+            this.inputPop.id = "plan_input"
+            this.inputPop.style.display = "flex";
+            this.inputPop.style.width = "300px";
+            this.inputPop.style.height = "30px";
+            this.inputPop.style.border = "1px solid";
+            this.inputPop.style.borderRadius = "5px";
 
-        textArea.addEventListener("keydown", (event) => {
-            if (event.key === "Enter" && !event.shiftKey) {
-                event.preventDefault();
-                save.click();
-            }
-        });
+            this.inputWrapper.appendChild(this.inputPop);
 
-    } else if (element.id === "plan_price" || element.id === "price_with_tax") {
-        const inputNum = document.createElement("input");
-        inputNum.type = "number";
-        inputNum.name = "plan_title";
-        inputNum.placeholder = label;
-        inputNum.id = "plan_input_number"
-        inputNum.style.display = "flex";
-        inputNum.style.width = "300px";
-        inputNum.style.height = "30px";
-        inputNum.style.border = "1px solid";
-        inputNum.style.borderRadius = "5px";
-
-        inputWrapper.appendChild(inputNum);
-        setTimeout(() => inputNum.focus(), 0);
-
-        inputNum.addEventListener("keydown", (event) => {
-            if (event.key === "Enter" && !event.shiftKey) {
-                event.preventDefault();
-                save.click();
-            }
-        });
-    } else if (element.id === "plan_img_url") {
-        const inputUrl = document.createElement("input");
-        inputUrl.type = "url";
-        inputUrl.name = "plan_image_url";
-        inputUrl.placeholder = label;
-        inputUrl.id = "plan_input_url";
-        inputUrl.style.display = "flex";
-        inputUrl.style.width = "300px";
-        inputUrl.style.height = "30px";
-        inputUrl.style.border = "1px solid";
-        inputUrl.style.borderRadius = "5px";
-
-        inputWrapper.appendChild(inputUrl);
-        setTimeout(() => inputUrl.focus(), 0);
-
-        inputUrl.addEventListener("keydown", (event) => {
-            if (event.key === "Enter" && !event.shiftKey) {
-                event.preventDefault();
-                save.click();
-            }
-        });
-    } else {
-        const input = document.createElement("input");
-        input.type = "text";
-        input.name = "plan_title";
-        input.placeholder = label;
-        input.id = "plan_input"
-        input.style.display = "flex";
-        input.style.width = "300px";
-        input.style.height = "30px";
-        input.style.border = "1px solid";
-        input.style.borderRadius = "5px";
-
-        inputWrapper.appendChild(input);
-        setTimeout(() => input.focus(), 0);
-
-        input.addEventListener("keydown", (event) => {
-            if (event.key === "Enter" && !event.shiftKey) {
-                event.preventDefault();
-                save.click();
-            }
-        });
-    }
-
-    const save = document.createElement("button");
-    save.innerText = "Save";
-    save.style.position = "absolute";
-    save.style.bottom = "0";
-    save.style.right = "0";
-    save.style.width = "100px";
-    save.style.height = "30px";
-    save.style.border = "1px solid";
-    save.style.borderRadius = "10px";
-    save.style.color = "black";
-    save.style.cursor = "pointer";
-
-
-    inputWrapper.appendChild(removeImage);
-    inputWrapper.appendChild(save);
-    document.body.appendChild(inputWrapper);
-
-    setTimeout(() => {
-        document.addEventListener("click", handleOutsideClick);
-    }, 0);
-
-    function handleOutsideClick(event) {
-        if (!inputWrapper.contains(event.target)) {
-            closePopUp();
         }
+
+        const inputToFocus = this.descriptionPop || this.pricePop || this.urlPop || this.inputPop;
+
+        if (inputToFocus) {
+            setTimeout( () => {
+                inputToFocus.focus();
+            }, 0);
+            inputToFocus.addEventListener("keydown", (event) => {
+                if (event.key === "Enter" && !event.shiftKey) {
+                    event.preventDefault();
+                    this.save(element);
+                }
+            });
+        }
+
+        this.saveButton = document.createElement("button");
+        this.saveButton.innerText = "Save";
+        this.saveButton.style.position = "absolute";
+        this.saveButton.style.bottom = "0";
+        this.saveButton.style.right = "0";
+        this.saveButton.style.width = "100px";
+        this.saveButton.style.height = "30px";
+        this.saveButton.style.border = "1px solid";
+        this.saveButton.style.borderRadius = "10px";
+        this.saveButton.style.color = "black";
+        this.saveButton.style.cursor = "pointer";
+
+
+        this.inputWrapper.appendChild(this.removeImage);
+        this.inputWrapper.appendChild(this.saveButton);
+        document.body.appendChild(this.inputWrapper);
+
+        this.handleOutsideClick = (event) => {
+            if ((!this.descriptionPop || !this.descriptionPop.contains(event.target)) && 
+            (!this.inputPop || !this.inputPop.contains(event.target)) && 
+            (!this.pricePop || !this.pricePop.contains(event.target)) && 
+            (!this.urlPop || !this.urlPop.contains(event.target)) &&
+            !this.inputWrapper.contains(event.target)
+        ) {
+                this.close();
+            }
+        }; 
+
+        setTimeout(() => {
+            document.addEventListener("click", this.handleOutsideClick);
+        },0);
+
+        this.removeImage.addEventListener("click", () => this.close());
+
+        this.saveButton.addEventListener("click", () => this.save(element));
+
     }
 
-    function closePopUp() {
-        const wrapper = document.querySelector(".input_wrapper");
-        if (wrapper) wrapper.remove();
+    save(element) {
+        const value = 
+            this.descriptionPop?.value ||
+            this.pricePop?.value ||
+            this.urlPop?.value ||
+            this.inputPop?.value || "";
+
+        element.value = value;
+
+        review_plan_form(value, element);
+
+        this.close();
+        }
+
+    close() {
+        this.inputWrapper.remove();
+        document.removeEventListener("click", this.handleOutsideClick);
 
         document.getElementById("title_section").style.opacity = "1";
         document.getElementById("title_section").style.transition = "0.3s";
 
         document.getElementById("form_container").style.opacity = "1";
         document.getElementById("form_container").style.transition = "0.3s";
-
-        document.removeEventListener("click", handleOutsideClick);
-
     }
 
-    removeImage.addEventListener("click", closePopUp);
 
-    if (element.id === "plan_description") {
-        document.querySelector("#plan_input_textarea").addEventListener("input", function () {
-            element.value = this.value;
-            review_plan_form(this.value, element);
-        });
-    } else if (element.id === "plan_price" || element.id === "price_with_tax") {
-        document.querySelector("#plan_input_number").addEventListener("input", function () {
-            element.value = this.value;
-            review_plan_form(this.value, element);
-        });
-    } else if (element.id === "plan_img_url") {
-        document.querySelector("#plan_input_url").addEventListener("input", function () {
-            element.value = this.value;
-            review_plan_form(this.value, element);
-        });
-    } else {
-        document.querySelector("#plan_input").addEventListener("input", function () {
-            element.value = this.value;
-            review_plan_form(this.value, element);
-        });
-    }
-
-    save.addEventListener("click", () => {
-        const inputWrapper = document.querySelector(".input_wrapper");
-        inputWrapper.parentNode.removeChild(inputWrapper);
-        document.getElementById("title_section").style.opacity = "1";
-        document.getElementById("title_section").style.transition = "0.3s";
-
-        document.getElementById("form_container").style.opacity = "1";
-        document.getElementById("form_container").style.transition = "0.3s";
-
-    })
 }
 
 
@@ -283,6 +257,12 @@ function showDetailPopUp() {
     removeImage2.style.right = "0";
     detailContainer2.appendChild(removeImage2);
 
+    detailContainer2.addEventListener("click", (event) => {
+        if (!event.currentTarget.contains(event.target)) {
+            closeDetailsPopup();
+        }
+    });
+
     setTimeout(() => {
         document.addEventListener("click", handleOutsideClick);
     }, 0);
@@ -304,9 +284,17 @@ function showDetailPopUp() {
         document.getElementById("form_container").style.transition = "0.3s";
 
         document.removeEventListener("click", handleOutsideClick);
-
+        
     }
 
+    document.getElementById("add_plan").addEventListener("click", (event) => {
+        event.preventDefault();
+        const { details, trcData, subData } = updatePlanReviewDetailsUpdate();
+        clearForms();
+        detailReview(details, trcData, subData);
+        closeDetailsPopup();
+    });
+    
 }
 
 const subtitleData = {
@@ -326,6 +314,8 @@ const reviewtrcData = {
     cormmencing_date: "none",
     expiry_date: "none"
 }
+
+const Plan = []
 
 function review_plan_form(value, element) {
 
@@ -351,15 +341,8 @@ function review_plan_form(value, element) {
         subtitleData[element.id] = value;
     }
 
-
-
 }
-document.getElementById("add_plan").addEventListener("click", (event) => {
-    event.preventDefault();
-    const { details, trcData, subData } = updatePlanReviewDetailsUpdate();
-    clearForms();
-    detailReview(details, trcData, subData);
-});
+
 
 const details = [];
 const trcData = [];
@@ -370,21 +353,24 @@ function updatePlanReviewDetailsUpdate() {
 
     if (subtitleData.plan_subtitle) subData.push(`<b>Plan Subtitle</b>: ${subtitleData.plan_subtitle}`);
 
-    if (reviewData.price_with_tax) details.push(`Price with TAX: ${reviewData.price_with_tax}`);
-    if (reviewData.voice) details.push(`Voice Any Net: ${reviewData.voice}`);
-    if (reviewData.sms) details.push(`SMS Any Net: ${reviewData.sms}`);
-    if (reviewData.data) details.push(`Any Time Data: ${reviewData.data}`);
+    if (reviewData.price_with_tax) details.push(`<b>Price with TAX</b>: ${reviewData.price_with_tax}`);
+    if (reviewData.voice) details.push(`<b>Voice Any Net</b>: ${reviewData.voice}`);
+    if (reviewData.sms) details.push(`<b>SMS Any Net</b>: ${reviewData.sms}`);
+    if (reviewData.data) details.push(`<b>Any Time Data</b>: ${reviewData.data}`);
 
     if (reviewtrcData.reference_no) trcData.push(`<b>TRC Reference Number</b>: ${reviewtrcData.reference_no}`);
     if (reviewtrcData.cormmencing_date) trcData.push(`<b>Commencing Data</b>: ${reviewtrcData.cormmencing_date}`);
     if (reviewtrcData.expiry_date) trcData.push(`<b>Expiry Data</b>: ${reviewtrcData.expiry_date}`);
+
+    console.log(details);
+    console.log(trcData);
+    console.log(subData);
 
     return { details, trcData, subData };
 }
 
 function detailReview(details, trcData, subData) {
 
-    console.log(subData);
 
     const reviewTable = document.getElementById("plan_review_table");
 
@@ -399,13 +385,20 @@ function detailReview(details, trcData, subData) {
 
     const reviewdetails = document.createElement("td");
     reviewdetails.id = "plan_review_details";
-    reviewdetails.innerHTML = `<b>${details.join("<br>")}</b>`;
+    reviewdetails.innerHTML = details.join("<br>");
     review_subtitle.appendChild(reviewdetails);
 
     const reviewTrc = document.createElement("td");
     reviewTrc.id = "plan_review_trcdetails";
     reviewTrc.innerHTML = trcData.join("<br>");
     review_subtitle.appendChild(reviewTrc);
+ 
+
+    details.length = 0;
+    trcData.length = 0;
+    subData.length = 0;
+
+    console.log(Plan);
 
     reviewTable.appendChild(review_subtitle);
 
